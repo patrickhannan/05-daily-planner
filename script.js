0f6d4b4c9b251ca473dde4384a4f9df047aa7887
Patrick Hannan <patrick@hannan.com>
2020-09-12 23:41:39 -0400

    Added a working save feature.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function () {
 
   $("#currentDay").text(today)
 
-  var workHours = ["9", "10", "11", "12", "13", "14", "15", "16", "17", "23", "0"]
+  var workHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"]
 
   for (var i = 0; i < workHours.length; i++) {
     var row = $("<div class='row'>")
@@ -20,13 +20,23 @@ $(function () {
         textInput.addClass("future");
       }
 
-    var saveButton = $("<button class='col-sm-1 saveBtn'>Save</button>")  
+    if (localStorage.getItem(workHours[i]) != null) {
+        textInput.text(localStorage.getItem(workHours[i]));
+      }
+
+    var saveButton = $("<button class='col-sm-1 saveBtn'>Save</button>");
+    saveButton.attr("data-time", workHours[i]);
 
     row.append(rowHour, textInput, saveButton);
     $(".container").append(row);
   }
 
-  
+    $(document).on("click", ".saveBtn", function (event) {
+        var storageHour = $(this).attr("data-time");
+        var storageText = $(this).siblings(".description").val();
+    
+        localStorage.setItem(storageHour, storageText);
+      });
 
 });
   
