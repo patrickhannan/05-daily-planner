3bdb5e06218bd913dad3c23d3ee5b2061a4c309d
Patrick Hannan <patrick@hannan.com>
2020-09-12 22:50:26 -0400

    Added a new var with hours that match those of the hour counter on momentjs, Added the ability tomake the text box have the class of past when the hour has passed.

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
 $(function () {
   var today = moment().format("dddd, MMMM Do YYYY")
-  var currentHour = moment().format("h a")  
+  var currentHour = moment().format("h")  
 
   $("#currentDay").text(today)
 
   var workHours = {
-      amHours: [9, 10, 11, 12],
-      pmHours: [1, 2, 3, 4, 5]
+      amHours: [9, 10, 11],
+      pmHours: [12, 1, 2, 3, 4, 5]
   }
-   
+
+  var militaryHours = [9, 10, 11, 12, 13, 14, 15, 16, 17]
 
   for (var i = 0; i < workHours.amHours.length; i++) {
-      var row = $("<div class='row'>")
-      var rowHour = $("<div class='col-sm-1 hour'>").text(workHours.amHours[i] + "AM")
-      var textInput = $("<textarea class='description col-sm-10 time-block'>")
-    
+    var row = $("<div class='row'>")
+    var rowHour = $("<div class='col-sm-1 hour'>").text(workHours.amHours[i] + "AM")
+    var textInput = $("<textarea class='description col-sm-10 time-block'>")
+    textInput.attr("data-time", workHours.amHours[i]);
 
+    if (textInput.attr("data-time") < militaryHours) {
+          textInput.addClass("past")
+    }
       
 
-      row.append(rowHour, textInput);
-      $(".container").append(row);
+    row.append(rowHour, textInput);
+    $(".container").append(row);
   }
 
   for (var i = 0; i < workHours.pmHours.length; i++) {
     var row = $("<div class='row'>")
     var rowHour = $("<div class='col-sm-1 hour'>").text(workHours.pmHours[i] + "PM")
     var textInput = $("<textarea class='description col-sm-10 time-block'>")
+    textInput.attr("data-time", workHours.pmHours[i]);
 
-
+    if (textInput.attr("data-time") < militaryHours) {
+        textInput.addClass("past")
+    }
 
 
     row.append(rowHour, textInput);
     $(".container").append(row);
   }
+
+  
 });
   
 
