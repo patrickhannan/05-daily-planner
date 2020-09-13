03d9c6dfd2074f35f79f86e4b040568095ea3e60
Patrick Hannan <patrick@hannan.com>
2020-09-12 22:40:38 -0400

    Updated the hour column so that it shows whether it is am or pm. Added the base code for the textarea.

--- script.js
+++ script.js
@@ -4,21 +4,33 @@ $(function () {
 
   $("#currentDay").text(today)
 
-  var workHours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
+  var workHours = {
+      amHours: [9, 10, 11, 12],
+      pmHours: [1, 2, 3, 4, 5]
+  }
+   
 
-  for (var i = 0; i < workHours.length; i++) {
+  for (var i = 0; i < workHours.amHours.length; i++) {
       var row = $("<div class='row'>")
-      var rowHour = $("<div class='col-sm-1 hour'>")
-
-      if (i < 2) {
-          rowHour.text(workHours[i] + " PM")
-      } else {
-          rowHour.text(workHours[i] + " AM")
-      }
+      var rowHour = $("<div class='col-sm-1 hour'>").text(workHours.amHours[i] + "AM")
+      var textInput = $("<textarea class='description col-sm-10 time-block>")
+    
 
       
 
-      row.append(rowHour);
+      row.append(rowHour, textInput);
+      $(".container").append(row);
+  }
+
+  for (var i = 0; i < workHours.pmHours.length; i++) {
+    var row = $("<div class='row'>")
+    var rowHour = $("<div class='col-sm-1 hour'>").text(workHours.pmHours[i] + "PM")
+    var textInput = $("<textarea class='description col-sm-10 time-block>")
+
+
+
+    
+    row.append(rowHour, textInput);
     $(".container").append(row);
   }
 });
