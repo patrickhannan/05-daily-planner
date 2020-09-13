2584fe7b39634e48e4e773aa4548b8f023913e0e
Patrick Hannan <patrick@hannan.com>
2020-09-12 23:44:44 -0400

    Added work hours in am and pm rather than military time.

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ $(function () {
 
   var workHours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"]
 
+  var realHours = ["9 AM", "10 AM", "11 AM", "12 PM", "1 PM", "2 PM", "3 PM", "4 PM", "5 PM"]
+
   for (var i = 0; i < workHours.length; i++) {
     var row = $("<div class='row'>")
-    var rowHour = $("<div class='col-sm-1 hour'>").text(workHours[i] + ":00")
+    var rowHour = $("<div class='col-sm-1 hour'>").text(realHours[i])
     var textInput = $("<textarea class='description col-sm-10 time-block'>");
     textInput.attr("data-time", workHours[i]);
 
