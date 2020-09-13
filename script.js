83e2a75b6eda4b53f5d2ed6a8e8f358e597a59b6
Patrick Hannan <patrick@hannan.com>
2020-09-12 22:28:55 -0400

    Added time column.

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
 $(function () {
   var today = moment().format("dddd, MMMM Do YYYY")
-  var currentHour = moment().format("HH")  
+  var currentHour = moment().format("h a")  
 
   $("#currentDay").text(today)
-})
+
+  var workHours = ["9", "10", "11", "12", "1", "2", "3", "4", "5"]
+
+  for (var i = 0; i < workHours.length; i++) {
+      var row = $("<div class='row'>")
+      var rowHour = $("<div class='col-sm-1 hour'>")
+
+      if (i < 2) {
+          rowHour.text(workHours[i] + " PM")
+      } else {
+          rowHour.text(workHours[i] + " AM")
+      }
+
+      
+
+      row.append(rowHour);
+    $(".container").append(row);
+  }
+});
+  
 
